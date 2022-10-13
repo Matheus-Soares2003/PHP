aed8aa2f1405d97ae1f0a6a51b18f040e85f0957
Matheus-Soares2003 <matheusstcabral@gmail.com>
2022-10-13 19:28:34 -0300

    Trabalhando com operadores aritmeticos

--- projeto02/operacoes.php
+++ projeto02/operacoes.php
@@ -12,10 +12,21 @@
         <div id="titulo">Curso de PHP para iniciantes - por Gustavo Guanabara</div>
         <div id="php">
             <?php
-                $num1 = 3;
-                $num2 = 2;
+                $num1 = $_GET["a"];
+                $num2 = $_GET["b"];
+
                 $soma = $num1 + $num2;
-                echo "A soma entre $num1 e $num2 é igual a $soma";
+                $subtracao = $num1 - $num2;
+                $mult = $num1 * $num2;
+                $divisao = $num1 / $num2;
+                $media = $soma / 2;
+
+                echo "<h2>Valores Recebidos: $num1 e $num2</h2>";
+                echo "<p>A soma entre é igual a $soma</p>";
+                echo "<p>A subtração é igual a $subtracao</p>";
+                echo "<p>A multiplicação é igual a $mult</p>";
+                echo "<p>A divisão entre é igual a $divisao</p>";
+                echo "<p>A média é igual a $media</p>";
             ?>
         </div>
     </div>
